b6e20866e868146b8dbbf86c6c7e220629e7ef56
superr <superr@ddayweb.com>
2015-06-08 11:48:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+android_device_lge_w5c
+======================
+
+TWRP for the LG Ultimate 2
---
+++ device_w5c.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := device/lge/w5c
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/lge/w5c/w5c-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/kernel/kernel/overlay
+
+LOCAL_PATH := device/lge/w5c
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel:kernel \
+
+# DT Image
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dt.img:dt.img \
+
+# Kernel Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:root/etc/twrp.fstab
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_w5c
+PRODUCT_DEVICE := w5c
